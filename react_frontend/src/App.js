83567ddb08c9ff9be3d4def0acee384f36913169
Mightysam01 <olaleyemightysam@yahoo.com>
2025-06-10 12:48:58 +0100

    render api added to react App.js

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
     setError('');
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://tweet-sentiment-api.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
